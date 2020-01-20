d3d00c1815c3395fd04be350f4c050d101feb92a
AlexLlanesQuevedo <59844946+AlexLlanesQuevedo@users.noreply.github.com>
2020-01-20 13:12:00 -0600

    Update TCGAcounter.sh

--- TCGAcounter.sh
+++ TCGAcounter.sh
@@ -1,8 +1,8 @@
 # Crea el directorio para guardar datos
-mkdir striatus
+mkdir melanerpes
 
 # Bajar las secuencias de cytb de Melanerpes striatus de NCBI
-curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&rettype=fasta&id=17834119,17834115,17834113" > striatus/striatus.fasta
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&rettype=fasta&id=17834119,17834115,17834113,2065540,60281185" > melanerpes/melanerpes.fasta
 
 # Contar el número de veces que aparece la secuencia TGCA en las secuencias descargadas
-grep "TGCA" striatus/striatus.fasta | wc -l
+grep "TGCA" melanerpes/melanerpes.fasta | wc -l
